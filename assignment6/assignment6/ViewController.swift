b82fa40d0af8ce6c03c02cdb03844b386f7c9de6
Christian <cmg580@utexas.edu>
2018-10-17 00:03:34 -0500

    Created Table View with the ability to add in members. Have not included persistent data. In the same format as the Lab.

--- assignment6/assignment6/ViewController.swift
+++ assignment6/assignment6/ViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "Adventurers"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
     }
-    var TestOne = "Testing"
-
+    
+    var names : [String] = []
+    
+    @IBAction func addMember(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController( title: "New Name",
+                                       message: "Add a new name",
+                                       preferredStyle: .alert )
+        
+        let saveAction = UIAlertAction( title: "Save",
+                                        style: .default)
+        {
+            [unowned self] action in
+                                            
+            guard let textField = alert.textFields?.first,
+                let nameToSave = textField.text else
+                {
+                return
+                }
+                                            
+            self.names.append(nameToSave)
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction ( title: "Back",
+                                           style: .cancel)
+    
+    alert.addTextField()
+    
+    alert.addAction(saveAction)
+    alert.addAction(cancelAction)
+    
+    present(alert, animated: true)
+    }
 }
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            cell.textLabel?.text = names[indexPath.row]
+            return cell
+    }
+}
+
 
