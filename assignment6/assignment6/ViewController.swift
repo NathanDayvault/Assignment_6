08ed07027088909248ce974a3e754e05a552d919
Christian <cmg580@utexas.edu>
2018-10-17 10:38:29 -0500

    Added in persistent data. The current app is a duplicate of the Lab.

--- assignment6/assignment6/ViewController.swift
+++ assignment6/assignment6/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
@@ -20,8 +21,30 @@ class ViewController: UIViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Adventurer")
+        
+        //3
+        do
+        {
+            members = try managedContext.fetch(fetchRequest)
+        }
+        catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
     
-    var names : [String] = []
+
+    
+    var members: [NSManagedObject] = []
     
     @IBAction func addMember(_ sender: UIBarButtonItem) {
         
@@ -29,20 +52,19 @@ class ViewController: UIViewController {
                                        message: "Add a new name",
                                        preferredStyle: .alert )
         
-        let saveAction = UIAlertAction( title: "Save",
-                                        style: .default)
+        let saveAction = UIAlertAction (title: "Save", style: .default)
         {
             [unowned self] action in
-                                            
+            
             guard let textField = alert.textFields?.first,
-                let nameToSave = textField.text else
-                {
-                return
-                }
-                                            
-            self.names.append(nameToSave)
+                let nameToSave = textField.text else {
+                    return
+            }
+            
+            self.save(name: nameToSave)
             self.tableView.reloadData()
         }
+
         
         let cancelAction = UIAlertAction ( title: "Back",
                                            style: .cancel)
@@ -54,21 +76,46 @@ class ViewController: UIViewController {
     
     present(alert, animated: true)
     }
+    
+    func save(name: String) {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Adventurer", in: managedContext)!
+        
+        let member = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        member.setValue(name, forKeyPath: "name")
+        
+        do
+        {
+            try managedContext.save()
+            members.append(member)
+        }
+        catch let error as NSError
+        {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
 }
 
 // MARK: - UITableViewDataSource
 extension ViewController: UITableViewDataSource {
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return members.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             
+            let member = members[indexPath.row]
             let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-            cell.textLabel?.text = names[indexPath.row]
+            cell.textLabel?.text = member.value(forKeyPath: "name") as? String
             return cell
     }
 }
+
 
 
